63b6b01d0f893a22c5bc1a05aeea80d4f71aedc2
jason-ziolo <jziolo@mymail.mines.edu>
2015-09-14 17:29:24 -0600

    Defined many methods in the Burner class

--- src/Burner.java
+++ src/Burner.java
@@ -1,34 +1,77 @@
 public class Burner {
   public enum Temperature {HOT, WARM, COLD};
   
-  private Temperature Temperature;
-  private Setting Setting;
+  private Temperature temperature;
+  private Setting setting;
   
   // TODO: Remove this warning suppression
   @SuppressWarnings("unused")
-  private int timer = -1;
+  private int timer = 0;
   final static int TIME_DURATION = 2;  // In minutes
   
   @SuppressWarnings("static-access")
   public Burner() {
     super();
-    this.Temperature = Temperature.COLD;
-    this.Setting = Setting.OFF;
+    this.temperature = Temperature.COLD;
+    this.setting = Setting.OFF;
   }
 
   public void increaseSetting() {
-    
+    timer += 2;
+    switch(setting) {
+    case OFF:
+    	setting = Setting.LOW;
+    	break;
+    case LOW:
+    	setting = Setting.MEDIUM;
+    	break;
+    case MEDIUM:
+    	setting = Setting.HIGH;
+    	break;
+    case HIGH:
+    	return;
+    }
   }
   
   public void decreaseSetting() {
-    
+    timer += 2;
+    switch(setting) {
+    case OFF:
+    	return;
+    case LOW:
+    	setting = Setting.OFF;
+    	break;
+    case MEDIUM:
+    	setting = Setting.LOW;
+    	break;
+    case HIGH:
+    	setting = Setting.MEDIUM;
+    	break;
+    }
   }
   
   public void updateTemperature() {
-    
+	timer--;
+	if(timer < 0) {
+		timer = 0;	// Might not be needed, but just in case
+	}
+    if(timer == 0) {
+    	switch(setting) {
+    	case OFF:
+    		temperature = Temperature.COLD;
+    		break;
+    	case LOW:
+    	case MEDIUM:
+    		temperature = Temperature.WARM;
+    		break;
+    	case HIGH:
+    		temperature = Temperature.HOT;
+    		break;
+    	}
+    }
   }
   
   public void display() {
-    
+    System.out.print("[" + setting.toString() + "].....");
   }
 }
