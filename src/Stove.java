cd8c88ae2bd2d22724d32986062d286586d31471
jason-ziolo <jziolo@mymail.mines.edu>
2015-09-14 19:07:57 -0600

    Finished tests 1-3

--- src/Stove.java
+++ src/Stove.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Timer;
+
 // testing
 /*
    TODO: Project Requirements:
@@ -17,6 +19,7 @@ import java.util.ArrayList;
     (6) Git log in two formats
 */
 
+
 public class Stove {
   private static final int DEFAULT_BURNERS = 4;
   ArrayList<Burner> burnerList = new ArrayList(DEFAULT_BURNERS);
@@ -26,6 +29,7 @@ public class Stove {
   }
 
   public void displayStove() {
+	System.out.println();
     System.out.println("Stove ---------------");
     boolean is_hot = false;
     for(Burner i : burnerList) {
@@ -40,10 +44,42 @@ public class Stove {
   }
   
   public static void main(String[] args) {
-    Stove stove = new Stove();
-    for(int i = 0; i < DEFAULT_BURNERS; i++) {
-    	stove.burnerList.add(new Burner());
-    }
-    stove.displayStove();
+	final long ONE_MINUTE = 1000; // TODO: Change back.
+	Stove stove = new Stove();
+	for(int i = 0; i < DEFAULT_BURNERS; i++) {
+		stove.burnerList.add(new Burner());
+	}
+	
+	stove.displayStove();
+	stove.burnerList.get(0).increaseSetting();
+	stove.burnerList.get(0).increaseSetting();
+	stove.burnerList.get(0).increaseSetting();
+	stove.burnerList.get(1).increaseSetting();
+	stove.burnerList.get(1).increaseSetting();
+	stove.burnerList.get(2).increaseSetting();
+	
+	long lastTime = System.currentTimeMillis();
+	
+	for(int i = 0; i < 20; i++) {
+		if((System.currentTimeMillis() - lastTime) < ONE_MINUTE) {
+			i = i - 1;
+			continue;
+		}
+		System.out.println("CURRENT: " + i);
+		for(Burner j : stove.burnerList) {
+			j.updateTemperature();
+		}
+		lastTime = System.currentTimeMillis();
+		if(i == 5) {
+			stove.displayStove();
+			stove.burnerList.get(0).decreaseSetting();
+			stove.burnerList.get(0).decreaseSetting();
+			stove.burnerList.get(1).increaseSetting();
+			stove.burnerList.get(2).decreaseSetting();
+		}
+		if(i == 9) {
+			stove.displayStove();
+		}
+	}
   }
 }
