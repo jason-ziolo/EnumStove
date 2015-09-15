9996d3bf4a02764d743883ee88ea78b588835016
jason-ziolo <jziolo@mymail.mines.edu>
2015-09-14 19:23:20 -0600

    finished all tests, elaborated more on testing

--- src/Burner.java
+++ src/Burner.java
@@ -4,12 +4,9 @@ public class Burner {
   private Temperature temperature;
   private Setting setting;
   
-  // TODO: Remove this warning suppression
-  @SuppressWarnings("unused")
   private int timer = 0;
   final static int TIME_DURATION = 2;  // In minutes
   
-  @SuppressWarnings("static-access")
   public Burner() {
     super();
     this.temperature = Temperature.COLD;
--- src/Stove.java
+++ src/Stove.java
@@ -1,21 +1,10 @@
 import java.util.ArrayList;
 import java.util.Timer;
 
-// testing
 /*
    TODO: Project Requirements:
-   
-    (5) Setting enumerated type
-    Burner class with:
-    (3) Temperature enumerated type and variable, with getter
-    (1) Constructor
-    (3) increase and decrease setting methods with switch statement
-    (3) updateTemperature method
-    (2) display method
+
     Stove class with:
-    (1) ArrayList of burners
-    (1) Constructor
-    (5) Methods to reproduce test display, with a comment explaining how you tested.
     (6) Git log in two formats
 */
 
@@ -44,41 +33,49 @@ public class Stove {
   }
   
   public static void main(String[] args) {
-	final long ONE_MINUTE = 1000; // TODO: Change back.
+	final long ONE_MINUTE = 60000; // In milliseconds. Was changed to 1000 for testing.
 	Stove stove = new Stove();
 	for(int i = 0; i < DEFAULT_BURNERS; i++) {
 		stove.burnerList.add(new Burner());
 	}
 	
-	stove.displayStove();
-	stove.burnerList.get(0).increaseSetting();
-	stove.burnerList.get(0).increaseSetting();
-	stove.burnerList.get(0).increaseSetting();
+	stove.displayStove();						// The burners all start out cold, and off.
+	stove.burnerList.get(0).increaseSetting();	
+	stove.burnerList.get(0).increaseSetting();	// All of the burners are set to the setting
+	stove.burnerList.get(0).increaseSetting();	// that they will be tested at next.
 	stove.burnerList.get(1).increaseSetting();
 	stove.burnerList.get(1).increaseSetting();
 	stove.burnerList.get(2).increaseSetting();
 	
 	long lastTime = System.currentTimeMillis();
 	
-	for(int i = 0; i < 20; i++) {
-		if((System.currentTimeMillis() - lastTime) < ONE_MINUTE) {
-			i = i - 1;
-			continue;
+	for(int i = 0; i < 12; i++) {
+		if((System.currentTimeMillis() - lastTime) < ONE_MINUTE) {	// Tests to see if a minute has elapsed
+			i = i - 1;	// The continue statement will cause i to increment,
+			continue;	// which we only want to happen every minute.
 		}
-		System.out.println("CURRENT: " + i);
-		for(Burner j : stove.burnerList) {
+		
+		for(Burner j : stove.burnerList) {	// Range-based, updates all burners.
 			j.updateTemperature();
 		}
-		lastTime = System.currentTimeMillis();
-		if(i == 5) {
-			stove.displayStove();
+		
+		lastTime = System.currentTimeMillis();	// After updating, set our variable to the current time
+		if(i == 5) {				// After 6 iterations of 1 minute each, we display the 
+			stove.displayStove();	// stove and update the settings for the next test.
 			stove.burnerList.get(0).decreaseSetting();
 			stove.burnerList.get(0).decreaseSetting();
 			stove.burnerList.get(1).increaseSetting();
 			stove.burnerList.get(2).decreaseSetting();
 		}
-		if(i == 9) {
+		
+		if(i == 9) {				// After 10 iterations, same as above.
 			stove.displayStove();
+			stove.burnerList.get(0).decreaseSetting();
+			stove.burnerList.get(1).decreaseSetting();
+		}
+		
+		if(i == 11) {				// After 12 iterations, all tests are done...
+			stove.displayStove();	// for loop will exit after i is incremented to 12.
 		}
 	}
   }
